89574abd0e133843d7d213c857f6d4860de16b18
Loic R <rouquette.loic@gmail.com>
2018-10-05 22:50:08 +0200

    Add querying interface

---
+++ src/main/java/com/github/quadinsa5if/findingandqueryingtext/service/Prophet.java
@@ -0,0 +1,16 @@
+package com.github.quadinsa5if.findingandqueryingtext.service;
+
+import com.github.quadinsa5if.findingandqueryingtext.lang.Iter;
+import com.github.quadinsa5if.findingandqueryingtext.model.ArticleId;
+
+public interface Prophet {
+
+  /**
+   * Return the top k articles where the terms have the better score
+   * @param terms The list of term to search
+   * @param k The number of articles to return
+   * @return An iterator other the articles
+   */
+  Iter<ArticleId> answer(String[] terms, int k);
+
+}
