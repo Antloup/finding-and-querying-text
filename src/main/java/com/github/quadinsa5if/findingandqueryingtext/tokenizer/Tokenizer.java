0e6e5c7cda02fa7844e7ada9617a83bc2c6b27e8
Loic R <rouquette.loic@gmail.com>
2018-10-05 15:06:15 +0200

    Add NotNull to Tokenizer

--- src/main/java/com/github/quadinsa5if/findingandqueryingtext/tokenizer/Tokenizer.java
+++ src/main/java/com/github/quadinsa5if/findingandqueryingtext/tokenizer/Tokenizer.java
@@ -1,7 +1,8 @@
 package com.github.quadinsa5if.findingandqueryingtext.tokenizer;
 
 import com.github.quadinsa5if.findingandqueryingtext.lang.Iter;
+import org.jetbrains.annotations.NotNull;
 
 public interface Tokenizer<I, O, E extends Exception> {
-  Iter<O> tokenize(I input) throws E;
+  Iter<O> tokenize(@NotNull I input) throws E;
 }
