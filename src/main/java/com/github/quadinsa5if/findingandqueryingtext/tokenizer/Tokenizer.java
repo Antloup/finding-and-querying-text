72cc94d7eb8a01ddb0a43fe98f7eaeb183135d33
Loic R <rouquette.loic@gmail.com>
2018-10-12 14:27:07 +0200

    Remove unused files

--- src/main/java/com/github/quadinsa5if/findingandqueryingtext/tokenizer/Tokenizer.java
+++
@@ -1,8 +0,0 @@
-package com.github.quadinsa5if.findingandqueryingtext.tokenizer;
-
-import com.github.quadinsa5if.findingandqueryingtext.lang.Iter;
-import org.jetbrains.annotations.NotNull;
-
-public interface Tokenizer<I, O, E extends Exception> {
-  Iter<O> tokenize(@NotNull I input) throws E;
-}
