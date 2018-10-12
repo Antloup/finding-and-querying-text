96c2b295da7d8176130379fad1c1b08023c62596
YanisGhernouz <yanis.ghernouz@insa-lyon.fr>
2018-10-12 15:09:58 +0200

    merger

--- src/main/java/com/github/quadinsa5if/findingandqueryingtext/service/implementation/InvertedFileMergerImplementation.java
+++ src/main/java/com/github/quadinsa5if/findingandqueryingtext/service/implementation/InvertedFileMergerImplementation.java
@@ -1,4 +1,15 @@
 package com.github.quadinsa5if.findingandqueryingtext.service.implementation;
 
-public class InvertedFileMergerImplementation {
+import com.github.quadinsa5if.findingandqueryingtext.service.InvertedFileMerger;
+
+import java.io.File;
+import java.util.List;
+
+public class InvertedFileMergerImplementation implements InvertedFileMerger {
+    @Override
+    public File merge(List<File> parts){
+
+        File full_file;
+        return null;
+    }
 }
