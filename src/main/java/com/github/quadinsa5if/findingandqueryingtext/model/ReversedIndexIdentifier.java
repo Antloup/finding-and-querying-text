088c08eb8c3f377c288604e75766b90a3b5dab75
YanisGhernouz <yanis.ghernouz@insa-lyon.fr>
2018-10-14 16:35:38 +0200

    merger

--- src/main/java/com/github/quadinsa5if/findingandqueryingtext/model/ReversedIndexIdentifier.java
+++ src/main/java/com/github/quadinsa5if/findingandqueryingtext/model/ReversedIndexIdentifier.java
@@ -4,10 +4,15 @@ public class ReversedIndexIdentifier {
 
   public final int offset;
   public final int length;
+  public String term="";
 
   public ReversedIndexIdentifier(int offset, int length) {
     this.offset = offset;
     this.length = length;
   }
-
+  public ReversedIndexIdentifier(int offset, int length, String term) {
+    this.offset = offset;
+    this.length = length;
+    this.term  = term;
+  }
 }
