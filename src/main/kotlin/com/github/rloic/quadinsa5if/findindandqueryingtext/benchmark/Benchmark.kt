1fe320585a334de479286f8321cf5dd81433f730
Loic R <rouquette.loic@gmail.com>
2018-10-29 08:28:16 +0100

    Add heap monitor to Benchmarks

--- src/main/kotlin/com/github/rloic/quadinsa5if/findindandqueryingtext/benchmark/Benchmark.kt
+++ src/main/kotlin/com/github/rloic/quadinsa5if/findindandqueryingtext/benchmark/Benchmark.kt
@@ -8,33 +8,31 @@ class Benchmark {
             functionName: String,
             parameterName: String,
             parameters: List<T>,
-            iteration: Int = 100,
+            iterations: Int = 100,
             func: (T) -> Unit
     ) {
         val times = LongArray(parameters.size)
-        for (i in 0 until iteration) {
+        val heapUsages = LongArray(parameters.size)
+        val heapMonitor = HeapMonitor()
+
+        for (i in 0 until iterations) {
             var j = 0
             for (param in parameters) {
+                heapMonitor.reset()
                 times[j] += measureTimeMillis { func(param) }
+                heapUsages[j] += heapMonitor.heapUsage()
                 j += 1
             }
         }
 
-        println("Function: $functionName")
+        println("Function: $functionName for $iterations iterations")
         for (i in parameters.indices) {
             print("With parameter $parameterName = ${parameters[i]}, \t")
-            val score = "%.4f".format(times[i] / iteration.toDouble())
-            println("mean runtime = $score ms, for $iteration iterations")
+            val score = "%.4f".format(times[i] / iterations.toDouble())
+            val heapUsage = HeapMonitor.format(heapUsages[i] / iterations)
+            println("mean runtime = $score ms, mean heap usage = $heapUsage")
         }
         println("---------------------------------------")
     }
-
-}
-
-fun main(args: Array<String>) {
-
-
-    val benchmark = Benchmark()
-    benchmark.bench("addition", "x", listOf(1, 2, 3, 4, 5), iteration = 1000) { x -> x + 5 }
 
 }
