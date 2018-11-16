25d0e0c126ca97fde77a6ab7d19fef4f33a0ce69
Loic R <rouquette.loic@gmail.com>
2018-11-16 13:17:50 +0100

    Remove GC benchmark and add build IF benchmark to doc

--- src/main/kotlin/com/github/rloic/quadinsa5if/findindandqueryingtext/benchmark/Benchmark.kt
+++ src/main/kotlin/com/github/rloic/quadinsa5if/findindandqueryingtext/benchmark/Benchmark.kt
@@ -11,21 +11,17 @@ class Benchmark {
             iterations: Int = 100,
             func: (T) -> Unit
     ) {
-        val heapMonitor = HeapMonitor()
 
         println("Function: $functionName for $iterations iterations")
         for (param in parameters) {
             var time = 0L
-            var heapUsage = 0L
             for (i in 0 until iterations) {
-                heapMonitor.reset()
                 time += measureTimeMillis { func(param) }
-                heapUsage += heapMonitor.heapUsage()
             }
             print("With parameter $parameterName = $param, \t")
             val score = "%.4f".format(time / iterations.toDouble())
-            val meanHeap = HeapMonitor.format(heapUsage / iterations)
-            println("mean runtime = $score ms, mean heap usage = $meanHeap")
+
+            println("mean runtime = $score ms")
         }
         println("---------------------------------------")
     }
